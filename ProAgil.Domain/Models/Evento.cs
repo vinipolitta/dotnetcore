bc9d314f0978b3dc5f83499125c5ae8f1a9d87be
Vini <jewoci6850@awinceo.com>
2021-06-16 15:13:30 -0300

    Criado Entidades pra banco de dados

---
+++ ProAgil.Domain/Models/Evento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProAgil.Domain.Models;
+
+namespace ProAgil.Domain
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+        public string Local { get; set; }
+        public DateTime DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QntdPessoas { get; set; }
+        public string ImagemURL { get; set; }
+
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string Lote { get; set; }
+        public List<Lote> Lotes { get; set; }
+        public List<RedeSocial> RedeSociais { get; set; }
+        public List<PalestranteEvento> PalestranteEventos { get; set; }
+
+    }
+}
